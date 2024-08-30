5052ed5ee99f0920192f0703f9d974cc30ab3906
crls04 <carlosruizlambert04@gmail.com>
2024-08-30 17:45:42 -0400

    script compilerOrientation
    _este script es el que llamara a los metodos principales de las clases en los otros script para poder compilar correctamente

---
+++ Assets/Scripts/compilerOrientation.cs
@@ -0,0 +1,33 @@
+using cardMaker;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class compilerOrientation : MonoBehaviour
+{
+    public TMP_InputField Console, Output;
+
+    public void Build()
+    {
+        syntax process = new();
+        process.delete();
+        process.verifySyntax(Console.text);
+        Output.text = "";
+        if (process.errors.Count <= 0)
+        {
+           
+            Output.text += "Compilacion exitosa";
+
+        }
+
+        else
+        {
+            foreach ( compilerErrors lines in process.errors)
+            {
+                Output.text += "Line: " + lines.line + ": *" + lines.errors + "*" + "\n";
+            }
+        }
+
+    }
+}
